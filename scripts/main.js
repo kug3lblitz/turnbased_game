688063ba9a2b3b025b4f7c293c2c549e9fb84946
Patrick Werner <pwerner2@gmail.com>
2015-02-07 11:19:31 -0500

    added partial jcvd script

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
-// this is js
+//vars
+
+var dmgAmt = "";
+
+
+var JCVD = function() {
+  this.name = "jcvd";
+  this.health = 100;
+  this.dmg = 0;
+};
+
+var spaceLlama = function() {
+  this.name = "Astral Alpaca";
+  this.health = 100;
+  this.dmg = 0;
+};
+
+JCVD.prototype.attack1 = function(nemesis) {
+  if (Math.random() < 0.75 ) {
+	  dmgAmt = Math.floor(Math.random() * 20 + 1);
+	  nemesis.health = nemesis.health - dmgAmt;
+	  nemesis.dmg = dmgAmt;
+	  console.log("You Karate Chopped " + nemesis.name + " in the throat!" + dmgAmt + " damage!");
+			  } else {
+				 console.log("YOU MISSED! Prepare for a beatdown!");
+				 nemesis.dmg = 0;
+		}
+	};
+};
 
 
