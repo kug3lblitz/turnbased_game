3a4719277baeac6772cdfa74a68b623242603c06
Patrick Werner <pwerner2@gmail.com>
2015-02-09 09:34:33 -0500

    added add/remove blocks, not yet implemented

--- scripts/main.js
+++ scripts/main.js
@@ -67,5 +67,31 @@ astralAlpaca.prototype.attack = function(nemesis) {
 // Health Bars
 
 
+// add and remove functions
 
+function addElement() {
 
+  var ni = document.getElementById('myDiv');
+  var numi = document.getElementById('theValue');
+
+  var num = (document.getElementById('theValue').value -1)+ 2;
+    numi.value = num;
+
+  var newdiv = document.createElement('div');
+
+  var divIdName = 'my'+num+'Div';
+
+    newdiv.setAttribute('id',divIdName);
+
+    newdiv.innerHTML = 'Element Number '+num+' has been added! <a href=\'#\' onclick=\'removeElement('+divIdName+')\'>Remove the div "'+divIdName+'"</a>';
+
+    ni.appendChild(newdiv);
+
+}
+
+function removeElement(divNum) {
+  var d = document.getElementById('myDiv');
+  var olddiv = document.getElementById(divNum);
+    d.removeChild(olddiv);
+
+}
